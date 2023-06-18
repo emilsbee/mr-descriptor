007e79c6699060237ae5a8f3605e817b5782f624
Emils Bernhards <emilsbernhards2001@gmail.com>
2023-06-18 13:26:49 +0200

    Update

--- deps.js
+++ deps.js
@@ -1,2 +1,4 @@
 import { execSync } from 'child_process';
+import path from 'path';
+const __dirname = path.resolve();
 console.info(execSync('npm ci --ignore-scripts', { cwd: __dirname }).toString());
