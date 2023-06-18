3a4b80dcaa6d6c2ea5494f7c9994bd22dc2f43f9
Emils Bernhards <emilsbernhards2001@gmail.com>
2023-06-18 13:29:58 +0200

    Update

--- deps.js
+++ deps.js
@@ -1,5 +1,7 @@
 import { execSync } from 'child_process';
 import path from 'path';
+import fs from 'node:fs'
 const __dirname = path.resolve();
-console.error("HEREHERHEHRE: ", __dirname)
+const files = execSync('ls')
+console.error("HEREHERHEHRE: ", files)
 console.info(execSync('npm ci --ignore-scripts', { cwd: __dirname }).toString());
